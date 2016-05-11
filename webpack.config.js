9867eb40453f87f7275995598cb775c6f6a5314c
smallbellows <given.sara@gmail.com>
2016-05-11 23:13:50 +0000

    initial

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+var path = require('path');
+var webpack = require('webpack');
+module.exports = {
+    entry: {
+        main: './src/main.jsx'
+    },
+    output: {
+        filename: './build/main.js',
+        // publicPath: '/build/'
+    },
+    devtool: '#source-map',
+    devServer: {
+      watch:true,
+      inline: true,
+      host: '0.0.0.0',
+      port: '3000',
+      watchOptions: {
+            aggregateTimeout: 300,
+            poll: true
+      }
+    },
+    module: {
+         loaders: [
+            {
+                test: /\.(js|jsx)$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react', 'stage-0']
+                }
+            },
+            {
+                test: /\.scss$/,
+                loaders: ['style', 'css', 'sass']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        })
+    ]
+}
